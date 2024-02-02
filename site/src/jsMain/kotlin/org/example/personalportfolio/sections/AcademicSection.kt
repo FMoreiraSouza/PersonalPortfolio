8afb67f0e323620b7c32a8624eb11c4ffb9e9060
felip <felipemoreira512@outlook.com>
2024-02-01 22:30:14 -0300

    Mapeamento dos padrões de tela na estrutura de mobilidade das Animações

--- site/src/jsMain/kotlin/org/example/personalportfolio/sections/AcademicSection.kt
+++ site/src/jsMain/kotlin/org/example/personalportfolio/sections/AcademicSection.kt
@@ -43,7 +43,7 @@ fun AcademicContent() {
     var animatedOpacity by remember { mutableStateOf(0.percent) }
     ObserveViewportEntered(
         sectionId = Section.Academics.id,
-        distanceFromTop = if (breakpoint >= Breakpoint.MD && breakpoint <= Breakpoint.LG) 1200.0 else 500.0,
+        distanceFromTop = if (breakpoint > Breakpoint.LG || breakpoint < Breakpoint.MD) 500.0 else if (breakpoint > Breakpoint.MD || breakpoint <= Breakpoint.LG) 1300.0 else 1200.0,
         onViewportEntered = {
             scope.launch {
                 animatedMargin = 0.px
