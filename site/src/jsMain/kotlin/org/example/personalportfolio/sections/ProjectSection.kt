29dcfca1fc4a073017c358c9a38279547387a0b6
felip <felipemoreira512@outlook.com>
2024-02-01 21:17:11 -0300

    Reajustes de detalhes em Animações

--- site/src/jsMain/kotlin/org/example/personalportfolio/sections/ProjectSection.kt
+++ site/src/jsMain/kotlin/org/example/personalportfolio/sections/ProjectSection.kt
@@ -44,7 +44,7 @@ fun ProjectContent() {
     var animatedOpacity by remember { mutableStateOf(0.percent) }
     ObserveViewportEntered(
         sectionId = Section.Projects.id,
-        distanceFromTop = if (breakpoint > Breakpoint.LG || breakpoint < Breakpoint.MD) 500.0 else if (breakpoint > Breakpoint.MD || breakpoint <= Breakpoint.LG) 1200.0 else 1000.0,
+        distanceFromTop = if (breakpoint > Breakpoint.LG || breakpoint < Breakpoint.MD) 500.0 else 1200.0,
         onViewportEntered = {
             scope.launch {
                 animatedMargin = 0.px
